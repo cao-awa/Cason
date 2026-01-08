08b0769a22d002eb0d73ee988fbb3f96c61d3701
cao_awa <cao.awa.www@gmail.com>
2026-01-08 10:19:17 +0800

    More data stream method.

--- src/main/kotlin/com/github/cao/awa/cason/stream/DataStream.kt
+++ src/main/kotlin/com/github/cao/awa/cason/stream/DataStream.kt
@@ -1,11 +1,11 @@
 package com.github.cao.awa.cason.stream
 
 class DataStream<T>(private val rawData: T, private val finalize: (T) -> Unit) {
-    private var success: Boolean = true
+    private var validated: Boolean = true
     private var data: T = this.rawData
 
     infix fun validate(body: T.() -> Boolean): DataStream<T> {
-        this.success = this.success and body(this.data)
+        this.validated = this.validated and body(this.data)
 
         return this
     }
@@ -17,13 +17,31 @@ class DataStream<T>(private val rawData: T, private val finalize: (T) -> Unit) {
     }
 
     infix fun mapIfValidated(body: T.(T) -> T): DataStream<T> {
-        this.data = body(this.data, this.data)
+        if (this.validated) {
+            this.data = body(this.data, this.data)
+        }
+
+        return this
+    }
+
+    infix fun ifValidated(body: T.(T) -> Unit): DataStream<T> {
+        if (this.validated) {
+            body(this.data, this.data)
+        }
+
+        return this
+    }
+
+    infix fun ifUnvalidated(body: T.(T) -> Unit): DataStream<T> {
+        if (!this.validated) {
+            body(this.data, this.data)
+        }
 
         return this
     }
 
     fun commit() {
-        if (this.success) {
+        if (this.validated) {
             this.finalize(this.data)
         }
     }
